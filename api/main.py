b71ff7489608a725516c9e78b0f14f78d9f939e5
Spencer Cooley <contact@spencercooley.com>
2025-03-01 15:15:22 -0600

    reverted main.py to more simple file

--- api/main.py
+++ api/main.py
@@ -1,15 +1,7 @@
 from fastapi import Depends, FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-import os
 
-# Try to load environment variables from .env file
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-    print("Environment variables loaded from .env file")
-except ImportError:
-    print("python-dotenv not installed. Environment variables must be set manually.")
 
 import routers
 
@@ -27,9 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"]
 )
 
 
